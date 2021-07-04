d245eeca53889b6dc6f92a1128d238a862537c30
tm-j-chn <timchun0212@gmail.com>
2021-07-04 16:06:29 +0900

    urls need leading slash

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 HOME_PAGE = "https://got-todo-app.herokuapp.com"
 
 from posting.views import *
-app.register_blueprint(todo_bp, url_prefix = f"{HOME_PAGE}/post")
+app.register_blueprint(todo_bp, url_prefix = f"/{HOME_PAGE}/post")
 
 
 if __name__ == '__main__':
