bda261fec0703984e76a5c25894b93a27e75e070
tm-j-chn <timchun0212@gmail.com>
2021-07-05 17:50:19 +0900

    changed app to run

--- migrations/versions/431ec1686f9e_.py
+++ migrations/versions/431ec1686f9e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8d24673ba3fe
+Revision ID: 431ec1686f9e
 Revises: 
-Create Date: 2021-07-05 15:35:09.235621
+Create Date: 2021-07-05 17:49:56.891240
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8d24673ba3fe'
+revision = '431ec1686f9e'
 down_revision = None
 branch_labels = None
 depends_on = None
