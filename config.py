60440a3ee12ff4c073669fc04167224f6c391ef3
tm-j-chn <timchun0212@gmail.com>
2021-07-05 19:53:39 +0900

    changed name to sql

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = "postgres://opyosnsutmfjyb:9cae28d9613531aa063db87bf1c07d215dd9e184edb5250feac91c74074569d9@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8q45oit7uisbq"
+SQLALCHEMY_DATABASE_URI = "postgresql://opyosnsutmfjyb:9cae28d9613531aa063db87bf1c07d215dd9e184edb5250feac91c74074569d9@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8q45oit7uisbq"
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = "orangeisthenewblack01234"
