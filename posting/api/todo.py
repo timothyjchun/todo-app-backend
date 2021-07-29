9b47a59296e434fba4c31cb456841d7460d83db3
tm-j-chn <timchun0212@gmail.com>
2021-07-29 23:17:17 +0900

    finding reason of error 2

--- posting/api/todo.py
+++ posting/api/todo.py
@@ -5,18 +5,18 @@ import json
 
 class Todo(Resource):
     def get(self):
+        print("here")
         todo = db.session.query().with_entities(Test_Board.context).all()
         print(todo)
         return todo
         
     #for a POST request, get the new data and save it to the database
     def post(self):
-        print(type(json.loads(request.data)['text']))
-        # text = Test_Board(
-        #     context = json.loads(request.data)['text']
-        # )
-        # print(text)
-        # db.session.add(text)
-        # db.session.commit()
+        text = Test_Board(
+            context = json.loads(request.data)['text']
+        )
+        print(text)
+        db.session.add(text)
+        db.session.commit()
         
         
