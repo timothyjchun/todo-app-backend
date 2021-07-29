93278febba4b5a581911d0466cf9318d1c4a77f3
tm-j-chn <timchun0212@gmail.com>
2021-07-29 23:28:46 +0900

    finding reason of error 3

--- posting/api/todo.py
+++ posting/api/todo.py
@@ -5,10 +5,9 @@ import json
 
 class Todo(Resource):
     def get(self):
-        print("here")
         todo = db.session.query().with_entities(Test_Board.context).all()
         print(todo)
-        return todo
+        # return todo
         
     #for a POST request, get the new data and save it to the database
     def post(self):
