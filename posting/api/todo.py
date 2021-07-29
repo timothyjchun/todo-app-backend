ffb99d3c9368d256b187921eb561fbf085cfce48
tm-j-chn <timchun0212@gmail.com>
2021-07-29 22:28:04 +0900

    post request handled

--- posting/api/todo.py
+++ posting/api/todo.py
@@ -7,11 +7,13 @@ class Todo(Resource):
         todo = db.session.query().with_entities(Test_Board.context).all()
         print(todo)
         return todo
-
+        
+    #for a POST request, get the new data and save it to the database
     def post(self):
         text = Test_Board(
-            context = request.data
+            context = request.data.decode("utf-8")
         )
+        print(text)
         db.session.add(text)
         db.session.commit()
         
