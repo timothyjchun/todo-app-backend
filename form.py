b100cb28d5eaab25a496d60d49fcee1986d8e496
tm-j-chn <timchun0212@gmail.com>
2021-07-30 23:20:18 +0900

    changed schema field to color_option

--- form.py
+++ form.py
@@ -2,4 +2,4 @@ from marshmallow import Schema, fields
 
 class TodoSchema(Schema):
     context = fields.String(required = True, data_key = "text")
-    option = fields.String(required = True, data_key = "color")
+    color_option = fields.String(required = True, data_key = "color")
