23b93908248a7ce6311ff251c3d0b18b4519f17d
tm-j-chn <timchun0212@gmail.com>
2021-08-01 15:31:23 +0900

    return id fixed

--- form.py
+++ form.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
 
 class TodoSchema(Schema):
+    id = fields.Integer(data_key = "id")
     context = fields.String(required = True, data_key = "text")
     color_option = fields.String(required = True, data_key = "color")
