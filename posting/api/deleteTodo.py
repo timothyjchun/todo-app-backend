35878a354db08a535b9de8a7ac5e1c78a7def804
tm-j-chn <timchun0212@gmail.com>
2021-08-01 16:31:04 +0900

    delete issuse fix 1

--- posting/api/deleteTodo.py
+++ posting/api/deleteTodo.py
@@ -9,8 +9,10 @@ import json
 class DeleteTodo(Resource):
     def post(self):
         data = json.loads(request.data)
+        print(f"data: {data}")
         task = Test_Board.query.filter_by(id = data["id"]).first()
-        db.session.delete(task)
-        db.session.commit()
+        print(f"task: {task}")
+        # db.session.delete(task)
+        # db.session.commit()
 
 
