cb740b9947ff64c60732945fb18a9ad71c10b503
Timothy Chun <timothychun@Timothys-MacBook-Air.local>
2021-08-04 18:29:05 +0900

    config

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
-
+import configparser
 
 SQLALCHEMY_DATABASE_URI = "postgresql://opyosnsutmfjyb:9cae28d9613531aa063db87bf1c07d215dd9e184edb5250feac91c74074569d9@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d8q45oit7uisbq"
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = os.environ.get('TODOAPP_SECRET_KEY',None)
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+SECRET_KEY = config['HOST']['SECRET_KEY']
+print(SECRET_KEY)
 
 
 
